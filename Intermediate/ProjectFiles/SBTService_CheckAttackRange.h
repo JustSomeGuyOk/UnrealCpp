889de1c3bdb1531359c2dcc5dfce6f009923f0d1
JustSomeGuyOk <116080120+JustSomeGuyOk@users.noreply.github.com>
2023-02-20 10:02:59 +0000

    Behavior

--- Intermediate/ProjectFiles/SBTService_CheckAttackRange.h
+++ Intermediate/ProjectFiles/SBTService_CheckAttackRange.h
@@ -14,5 +14,9 @@ class UNREALCPP_API USBTService_CheckAttackRange : public UBTService
 	GENERATED_BODY();
 
 protected:
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector AttackRangeKey;
+
 	virtual void TickNode(UBehaviourTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds);
 }
